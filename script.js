78fac1c26ea51d4ca3814c99a6ff37fe06093f04
mindunlockedofc <mindunlockedofc@gmail.com>
2024-07-03 21:55:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.container')
+    elements.forEach(element => {
+        element.classList.add('fade')
+    })
+});
